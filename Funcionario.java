811d3f61e35f0b0dce5ffc8251cf6d32c9fcdc56
Anderson Marinho <marinho.1983@gmail.com>
2024-07-07 19:40:45 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -6,45 +6,63 @@ public class Funcionario {
     private LocalDate dataDemissao;
     private double salarioBase;
     private double outrosDescontos;
+    private double feriasVencidas;
+    private double indenizacaoAdicional;
+    private double multaAtrasoPagamento;
+    private TipoRescisao tipoRescisao;
+    private int diasAvisoPrevio;
 
-    // Getters e Setters
-    public String getNome() {
-        return nome;
+    public Funcionario(String nome, LocalDate dataAdmissao, LocalDate dataDemissao, double salarioBase, double outrosDescontos, double feriasVencidas, double indenizacaoAdicional, double multaAtrasoPagamento, TipoRescisao tipoRescisao, int diasAvisoPrevio) {
+        this.nome = nome;
+        this.dataAdmissao = dataAdmissao;
+        this.dataDemissao = dataDemissao;
+        this.salarioBase = salarioBase;
+        this.outrosDescontos = outrosDescontos;
+        this.feriasVencidas = feriasVencidas;
+        this.indenizacaoAdicional = indenizacaoAdicional;
+        this.multaAtrasoPagamento = multaAtrasoPagamento;
+        this.tipoRescisao = tipoRescisao;
+        this.diasAvisoPrevio = diasAvisoPrevio;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    // Getters
+    public String getNome() {
+        return nome;
     }
 
     public LocalDate getDataAdmissao() {
         return dataAdmissao;
     }
 
-    public void setDataAdmissao(LocalDate dataAdmissao) {
-        this.dataAdmissao = dataAdmissao;
-    }
-
     public LocalDate getDataDemissao() {
         return dataDemissao;
-    }
-
-    public void setDataDemissao(LocalDate dataDemissao) {
-        this.dataDemissao = dataDemissao;
     }
 
     public double getSalarioBase() {
         return salarioBase;
     }
 
-    public void setSalarioBase(double salarioBase) {
-        this.salarioBase = salarioBase;
-    }
-
     public double getOutrosDescontos() {
         return outrosDescontos;
     }
 
-    public void setOutrosDescontos(double outrosDescontos) {
-        this.outrosDescontos = outrosDescontos;
+    public double getFeriasVencidas() {
+        return feriasVencidas;
+    }
+
+    public double getIndenizacaoAdicional() {
+        return indenizacaoAdicional;
+    }
+
+    public double getMultaAtrasoPagamento() {
+        return multaAtrasoPagamento;
+    }
+
+    public TipoRescisao getTipoRescisao() {
+        return tipoRescisao;
+    }
+
+    public int getDiasAvisoPrevio() {
+        return diasAvisoPrevio;
     }
 }
