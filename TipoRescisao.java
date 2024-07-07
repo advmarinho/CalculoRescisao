e595cf56250def6ad6601f3606ae6a508465d73e
Anderson Marinho <marinho.1983@gmail.com>
2024-07-07 19:40:25 -0300

    Update TipoRescisao.java

--- TipoRescisao.java
+++ TipoRescisao.java
@@ -1,10 +1,20 @@
 public enum TipoRescisao {
-    SEM_JUSTA_CAUSA,
-    POR_JUSTA_CAUSA,
-    PEDIDO_DE_DEMISSAO,
-    TERMINO_CONTRATO_EXPERIENCIA,
-    ACORDO_ENTRE_AS_PARTES,
-    RESCISAO_INDIRETA,
-    FALECIMENTO_DO_EMPREGADO,
-    RESCISAO_POR_CULPA_RECIPROCA
+    SEM_JUSTA_CAUSA("Sem Justa Causa"),
+    POR_JUSTA_CAUSA("Por Justa Causa"),
+    PEDIDO_DE_DEMISSAO("Pedido de Demissão"),
+    TERMINO_CONTRATO_EXPERIENCIA("Término de Contrato de Experiência"),
+    ACORDO_ENTRE_AS_PARTES("Acordo entre as Partes"),
+    RESCISAO_INDIRETA("Rescisão Indireta"),
+    FALECIMENTO_DO_EMPREGADO("Falecimento do Empregado"),
+    RESCISAO_POR_CULPA_RECIPROCA("Rescisão por Culpa Recíproca");
+
+    private final String descricao;
+
+    TipoRescisao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
